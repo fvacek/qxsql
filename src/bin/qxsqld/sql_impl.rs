40b751f6709a5f3e4519dad2a4d3ee1966a81f80
Fanda Vacek <fanda.vacek@gmail.com>
2026-06-11 09:15:04 +0200

    Make client_command_sender async and await calls

--- src/bin/qxsqld/sql_impl.rs
+++ src/bin/qxsqld/sql_impl.rs
@@ -230,12 +230,13 @@ impl qxsql::QxSqlApi for QxSql {
     }
 }
 
+#[async_trait]
 impl qxsql::QxSqlApiRecChng for QxSql {
     fn filter_recchng(&self, recchng:RecChng) -> Option<RecChng>  {
         Some(recchng)
     }
 
-    fn client_command_sender(&self) -> Option<ClientCommandSender>  {
+    async fn client_command_sender(&self) -> Option<ClientCommandSender>  {
         self.1.clone()
     }
 }
--- src/sql/recchng.rs
+++ src/sql/recchng.rs
@@ -7,12 +7,12 @@ use crate::{RecChng, RecOp, Record};
 #[async_trait]
 pub trait QxSqlApiRecChng: super::QxSqlApi {
     fn filter_recchng(&self, recchng: RecChng) -> Option<RecChng>;
-    fn client_command_sender(&self) -> Option<ClientCommandSender>;
+    async fn client_command_sender(&self) -> Option<ClientCommandSender>;
 
     async fn create_record_with_recchng(&self, table: &str, record: &Record, issuer: Option<String>) -> anyhow::Result<i64> {
         let insert_id = self.create_record(table, record).await?;
         let recchng = RecChng {table: table.to_string(), id:insert_id, record:Some(record.clone()), op: RecOp::Insert, issuer };
-        if let Some(recchng) = self.filter_recchng(recchng) && let Some(client_cmd_tx) = self.client_command_sender() {
+        if let Some(recchng) = self.filter_recchng(recchng) && let Some(client_cmd_tx) = self.client_command_sender().await {
             let rec = to_rpcvalue(&recchng)?;
             client_cmd_tx.send_message(shvrpc::RpcMessage::new_signal("sql", "recchng").with_param(rec))?;
         }
@@ -22,7 +22,7 @@ pub trait QxSqlApiRecChng: super::QxSqlApi {
         let updated = self.update_record(table, id, record).await?;
         if updated {
             let recchng = RecChng {table: table.to_string(), id, record:Some(record.clone()), op: RecOp::Update, issuer };
-            if let Some(recchng) = self.filter_recchng(recchng) && let Some(client_cmd_tx) = self.client_command_sender() {
+            if let Some(recchng) = self.filter_recchng(recchng) && let Some(client_cmd_tx) = self.client_command_sender().await {
                 let rec = to_rpcvalue(&recchng)?;
                 client_cmd_tx.send_message(shvrpc::RpcMessage::new_signal("sql", "recchng").with_param(rec))?;
             }
@@ -33,7 +33,7 @@ pub trait QxSqlApiRecChng: super::QxSqlApi {
         let deleted = self.delete_record(table, id).await?;
         if deleted {
             let recchng = RecChng {table: table.to_string(), id, record:None, op: RecOp::Delete, issuer };
-            if let Some(recchng) = self.filter_recchng(recchng) && let Some(client_cmd_tx) = self.client_command_sender() {
+            if let Some(recchng) = self.filter_recchng(recchng) && let Some(client_cmd_tx) = self.client_command_sender().await {
                 let rec = to_rpcvalue(&recchng)?;
                 client_cmd_tx.send_message(shvrpc::RpcMessage::new_signal("sql", "recchng").with_param(rec))?;
             }
